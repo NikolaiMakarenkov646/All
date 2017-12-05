62b494f536d6c4612c16aa8e8a6b52ee1c71d687
Nikolai Makarenkov <nikolaim@codevalue.net>
2017-12-05 17:30:50 +0200

    .

---
+++ AbsDistinct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsDistinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { -5,-3,-1,0,3,6};
+
+            int minNumber = GetDistinctCount(arr);
+            Console.WriteLine("min number is: {0}", minNumber);
+            Console.Read();
+        }
+
+        private static int GetDistinctCount(int[] arr)
+        {
+            return arr.ToList().ConvertAll(x => Math.Abs(x)).Distinct().Count();
+        }
+    }
+}
