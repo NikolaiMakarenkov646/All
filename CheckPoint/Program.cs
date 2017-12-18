4db99147f33d4848c822e1a3a22cfb8c956108b4
Nikolai Makarenkov <nikolaim@codevalue.net>
2017-12-18 20:09:21 +0200

    .

---
+++ CheckPoint/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static Tree CreateTree()
+        {
+            return null;
+        }
+
+        private static Result SearchInTree(string message)
+        {
+            
+        }
+
+    }
+}
---
+++ CheckPoint/Tree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint
+{
+    public class Tree
+    {
+        public string Sender { get; set; }
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+    }
+
+   
+}
